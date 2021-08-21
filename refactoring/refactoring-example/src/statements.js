b444943675eb371e28e1b21d61d0f328a08286f3
lucsimao <lucasimao64@gmail.com>
2021-08-21 02:34:40 -0300

    feat: replate temp with query + extract function

--- refactoring/refactoring-example/src/statements.js
+++ refactoring/refactoring-example/src/statements.js
@@ -10,11 +10,7 @@ const statement = function (invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
+  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
@@ -60,6 +56,14 @@ const statement = function (invoice, plays) {
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(number / 100);
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
   }
 };
 
