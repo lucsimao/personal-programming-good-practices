d1abac694d6dc0e3dbdb91bac7b7f49622eedbaf
lucsimao <lucasimao64@gmail.com>
2021-08-21 21:55:45 -0300

    feat: change function declaration

--- refactoring/refactoring-example/src/createStatementData.js
+++ refactoring/refactoring-example/src/createStatementData.js
@@ -7,9 +7,12 @@ function createStatementData(invoice, plays) {
   return result;
 
   function enrichedPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -65,8 +68,9 @@ function createStatementData(invoice, plays) {
 }
 
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
 
