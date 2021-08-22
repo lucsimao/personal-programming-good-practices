ed108dd74d6d9f1cc62925b6d81e9e9d17c1ab6c
lucsimao <lucasimao64@gmail.com>
2021-08-21 22:25:02 -0300

    feat: inline variable

--- refactoring/refactoring-example/src/createStatementData.js
+++ refactoring/refactoring-example/src/createStatementData.js
@@ -13,7 +13,7 @@ function createStatementData(invoice, plays) {
     );
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
